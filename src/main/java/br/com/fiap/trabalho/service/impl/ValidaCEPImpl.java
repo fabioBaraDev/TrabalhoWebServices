5b1edff3185eca63da799c1e0f5ea3c05cafeb1f
FABIO BARA <Fabio.Bara@midway.com.br>
2020-05-11 03:28:22 -0300

    retorno da validação do Cep

--- src/main/java/br/com/fiap/trabalho/service/impl/ValidaCEPImpl.java
+++ src/main/java/br/com/fiap/trabalho/service/impl/ValidaCEPImpl.java
@@ -4,7 +4,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
-
 import br.com.fiap.trabalho.entity.Endereco;
 import br.com.fiap.trabalho.service.ValidaCEP;
 
@@ -13,17 +12,17 @@ public class ValidaCEPImpl implements ValidaCEP {
 
 	public Boolean validar(String cep) {
 		try {
-//			RestTemplate rest = new RestTemplate();
+			RestTemplate rest = new RestTemplate();
 	
-//			String url = "https://viacep.com.br/ws/" + cep + "/json/";
-//			ResponseEntity<Endereco> response = rest.getForEntity(url, Endereco.class);
-//
-//			if (response.getStatusCode().equals(HttpStatus.OK)) {
+			String url = "https://viacep.com.br/ws/" + cep + "/json/";
+			ResponseEntity<Endereco> response = rest.getForEntity(url, Endereco.class);
+
+			if (response.getStatusCode().equals(HttpStatus.OK)) {
 				return true;
-//			}
-//			return true;
+			}
+			return false;
 		} catch (Exception e) {
-			return true;
+			return false;
 		}
 	}
 }
