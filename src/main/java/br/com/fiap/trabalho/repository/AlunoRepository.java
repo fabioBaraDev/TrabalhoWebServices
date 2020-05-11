4d62c15541190d3f03145b0052aebe8c93f3ad42
FABIO BARA <Fabio.Bara@midway.com.br>
2020-05-11 01:29:12 -0300

    correção

--- src/main/java/br/com/fiap/trabalho/repository/AlunoRepository.java
+++ src/main/java/br/com/fiap/trabalho/repository/AlunoRepository.java
@@ -12,7 +12,7 @@ import br.com.fiap.trabalho.entity.Aluno;
 
 public interface AlunoRepository extends JpaRepository<Aluno, Integer> {
 
-	@Query(value = "SELECT t.id, t.nome, t.numero_cartao FROM tb_aluno t WHERE t.nome LIKE %:nome% ", nativeQuery = true)
+	@Query(value = "SELECT t.id, t.nome, t.numero_cartao, t.status_aluno FROM tb_aluno t WHERE t.nome LIKE %:nome% ", nativeQuery = true)
 	List<Aluno> findByName(@Param("nome") String nome);
 	
 	@Query(value = "SELECT t.status_aluno FROM tb_aluno t WHERE t.id = :id ", nativeQuery = true)
